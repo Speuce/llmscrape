5ccd16c9af640ec9cfc28df744ab70177e26887f
Speuce <mrk5199@gmail.com>
2024-03-07 09:32:44 -0600

    Add Perplexity Connector

---
+++ connectors/perplexityAI.ts
@@ -0,0 +1,24 @@
+import { Page } from "@playwright/test";
+
+export async function PerplexityAI(prompt: string, page: Page){
+    // find searchbox of type textarea and with placeholder 'Type a message'
+    const searchbox = await page.waitForSelector('textarea[placeholder="Ask anything..."]');
+    // type query
+    await searchbox?.type('Playwright');
+    // press enter
+    await searchbox?.press('Enter');
+    // wait 1 sec for ll to load
+    await page.waitForTimeout(2000);
+  
+  
+    // locate a button that has nested text 'Rewrite'
+    const button = await page.waitForSelector('button:has-text("Rewrite")');
+    // wait until button is visible
+    await button?.waitForElementState('visible', {timeout: 60000});
+  
+    // find cib-message with source='bot'
+    const botMessage = await page.$('div.prose');
+
+    const outputContent = await botMessage?.textContent();
+    return outputContent;
+}
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { BingCopilotChat } from '../connectors/bingChat';
+import { PerplexityAI } from '../connectors/perplexityAI';
 
 test('Bing Copilot Chat', async ({ page }) => {
   await page.goto('https://www.bing.com/chat');
@@ -10,4 +11,12 @@ test('Bing Copilot Chat', async ({ page }) => {
   expect(result!.length).toBeGreaterThan(0);
 });
 
+test('Perplexity AI', async ({ page }) => {
+  await page.goto('https://perplexity.ai/');
+  // todo fetch prompts from spreadsheet and iterate
+  const result = await PerplexityAI('Playwright', page);
+  // todo track results somewhere
+  expect(result).toBeDefined();
+  expect(result!.length).toBeGreaterThan(0);
+});
 
